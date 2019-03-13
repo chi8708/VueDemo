6089303b1d79dfe4f95da6c7029900e7ba560dda
chi8708 <983143462@qq.com>
2019-03-13 09:53:42 +0800

    vue.config.js全局配置

--- hello-world/vue.config.js
+++ hello-world/vue.config.js
@@ -1,5 +1,10 @@
+
 module.exports = {
     configureWebpack: {
-      devtool: 'source-map'
+      devtool: 'source-map'//vs code调试需此代码
+    },
+    devServer: {
+      port:8015,//客户端
+      proxy: 'http://localhost:4000'//服务端api
     }
   }
