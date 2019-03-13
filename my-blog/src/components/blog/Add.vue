5c3cdf7552a734276098153a7e4e10ccfc164a66
chi8708 <983143462@qq.com>
2019-03-13 15:17:43 +0800

    myblog 修改

--- my-blog/src/components/blog/Add.vue
+++ my-blog/src/components/blog/Add.vue
@@ -1,56 +1,125 @@
 <template>
   <div id="blogAdd">
-      <form action="">
-          <h2>添加博客</h2>
-          <div class="f-row">
-            <Label>标题</Label>
-            <input type="text" name="" id="" required/>
-          </div>
-          <div class="f-row">
-            <Label>内容</Label>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-          </div>
-         <div class="f-row">
-            <Label>分类</Label>
-            <input type="checkbox" />
-          </div>
-          <div class="f-row">
-            <Label>作者</Label>
-            <select name="" id="">
-                <option value=""></option>
-            </select>
-          </div>
-          <div>
-              <button>提交</button>
-          </div>
-      </form>
-      <hr>
-      <div class="preview">
-          <h3>博客预览</h3>
-          <Label>标题</Label>
-          <div></div>
-          <Label>内容</Label>
-          <div></div>
-          <Label>分类</Label>
-          <div></div>
-           <Label>作者</Label>
-          <div></div>
+   <div class="container">
+   <h3>添加博客</h3>
+    <form action id="addForm">
+      <div class="f-row">
+        <Label>标题</Label>
+        <input type="text" v-model="blog.title" required>
       </div>
+      <div class="f-row">
+        <Label>内容</Label>
+        <textarea cols="30" v-model="blog.body" rows="10"></textarea>
+      </div>
+      <div class="f-row">
+        <Label>分类</Label>
+        <input type="checkbox" id="ck1" value=".net" v-model="blog.category">.net
+        <input type="checkbox" id="ck2" value=".java" v-model="blog.category">java
+        <input type="checkbox" id="ck3" value=".node.js" v-model="blog.category">node.js
+        <input type="checkbox" id="ck4" value="vue.js" v-model="blog.category">vue.js
+        <input type="checkbox" id="ck5" value="react" v-model="blog.category">react
+      </div>
+      <div class="f-row">
+        <Label>作者</Label>
+        <select v-model="blog.author">
+          <option v-for="(item) in authors" v-bind:value="item.name" :key="item.id">{{item.name}}</option>
+        </select>
+      </div>
+      <div>
+        <button id="btnSave" type="button" @click="save">提交</button>
+      </div>
+    </form>
+    
+    <h3>博客预览</h3>
+    <hr>
+    <div class="preview">
+      <Label>标题</Label>
+      <div>{{blog.title}}</div>
+      <Label>内容</Label>
+      <div>{{blog.body}}</div>
+      <Label>分类</Label>
+      <div>{{categoryShow}}</div>
+      <Label>作者</Label>
+      <div>{{blog.author}}</div>
+    </div>
+   </div>
+  
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'blogAdd',
-  data(){
-      return{
-          blog:{}
+  name: "blogAdd",
+  data() {
+    return {
+      authors: [
+        { id: 1, name: "jack" },
+        { id: 2, name: "rose" },
+        { id: 3, name: "cts" }
+      ],
+      blog: {
+        category: [] //不添加此属性category为bool
       }
+    };
+  },
+  computed: {
+    categoryShow() {
+      if (this.blog.category.length <= 0) {
+        return "";
+      }
+      return this.blog.category;
+    }
+  },
+  methods: {
+    save() {
+      this.$http
+        .post("http://jsonplaceholder.typicode.com/posts", this.blog)
+        .then((response) => {
+          console.log(response);
+        });
+    }
   }
-}
+};
 </script>
 
 <style scoped>
+.container *{
+  box-sizing: border-box;
+}
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+#addForm{
+  padding: 10px;
+  border: 1px solid #555;
+}
+
+input[type="text"],
+label,
+select,
+textarea
+ {
+  margin-top:10px;
+  width: 100%;
+  padding: 5px;
+}
+input[type="checkbox"]{
+  display: inline-block;
+}
+textarea{
+ width: 100%;
+ height: 100px;
+}
+
+#btnSave{
+  margin-top:10px;
+  padding: 3px;
+  font-size: 18px;
+  background-color:#ff0000;
+  color: white;
+  width:80px;
+  border-radius: 8px;
+}
 </style>
--- my-blog/src/main.js
+++ my-blog/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import resource  from 'vue-resource'
 
 Vue.config.productionTip = false
 
+Vue.use(resource)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
