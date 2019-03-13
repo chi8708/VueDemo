c1fc6c97db7700b59069797548ed1326dc24b659
chi8708 <983143462@qq.com>
2019-03-13 15:17:21 +0800

    my-blog 添加

---
+++ my-blog/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <BlogAdd msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import BlogAdd from './components/blog/Add.vue'
+
+export default {
+  name: 'app',
+  components: {
+    BlogAdd
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ my-blog/src/components/blog/Add.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="blogAdd">
+      <form action="">
+          <h2>添加博客</h2>
+          <div class="f-row">
+            <Label>标题</Label>
+            <input type="text" name="" id="" required/>
+          </div>
+          <div class="f-row">
+            <Label>内容</Label>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+          </div>
+         <div class="f-row">
+            <Label>分类</Label>
+            <input type="checkbox" />
+          </div>
+          <div class="f-row">
+            <Label>作者</Label>
+            <select name="" id="">
+                <option value=""></option>
+            </select>
+          </div>
+          <div>
+              <button>提交</button>
+          </div>
+      </form>
+      <hr>
+      <div class="preview">
+          <h3>博客预览</h3>
+          <Label>标题</Label>
+          <div></div>
+          <Label>内容</Label>
+          <div></div>
+          <Label>分类</Label>
+          <div></div>
+           <Label>作者</Label>
+          <div></div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'blogAdd',
+  data(){
+      return{
+          blog:{}
+      }
+  }
+}
+</script>
+
+<style scoped>
+</style>
